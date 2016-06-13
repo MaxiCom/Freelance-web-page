3d7b669c9aafd879ce71ac09b4e2bf0b35585d09
Maxime Morlet <maximemorlet@hotmail.fr>
2016-06-13 17:56:19 +0200

    ok

--- app/scripts/scroll.js
+++ app/scripts/scroll.js
@@ -2,23 +2,23 @@ $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
 	
   //Top bar
-	if (scroll > 250)
+	if (scroll > $(document).height() * 0.08)
 		$("nav").addClass("scrolled");
-	else if (scroll < 250)
+	else if (scroll < $(document).height() * 0.8)
 		$("nav").removeClass("scrolled");
   
   //About btn
-  if (scroll > 500)
+  if (scroll > $(document).height() * 0.12)
     $("#About-btn").addClass("animated fadeInUp opacity");
 
   //Services boxes
-  if (scroll > 900){
+  if (scroll > $(document).height() * 0.27){
     $(".service-box-left").addClass("animated fadeInLeft opacity");
     $(".service-box-right").addClass("animated fadeInRight opacity");
   }
 
   //Contact boxes
-  if (scroll > 1500){
+  if (scroll > $(document).height() * 0.6){
     $(".contact-item-left").addClass("animated fadeInLeft opacity");
     $(".contact-item-middle").addClass("animated fadeInUp opacity");
     $(".contact-item-right").addClass("animated fadeInRight opacity");
